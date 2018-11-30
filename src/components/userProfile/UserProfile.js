323373e5e9d5239974c94a07e87ee28df36725a9
Carlos Levir <carloslevirfs@gmail.com>
2018-11-30 13:56:23 -0300

    Style Card Insertion

--- src/components/userProfile/UserProfile.js
+++ src/components/userProfile/UserProfile.js
@@ -1,26 +1,51 @@
 import React, { Component } from 'react';
 import UserRepos from '../userRepos/UserRepos'
 import Card from '@material-ui/core/Card';
-import { CardContent } from '@material-ui/core';
 import propTypes from 'prop-types'
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 import './userProfile.css'
 
 class UserProfile extends Component {
     render() {
         var userProfile = this.props.user ?
         (
-            <div>
-                <Card>
-                    <CardContent>
-                        <div>
-                            <img className="avatar-profile" src={this.props.user.avatar_url} alt="Profile Image"/>
-                            <h2>{this.props.user.login}</h2>
-                            <p>{this.props.user.name}</p>
-                            <p>Followers: {this.props.user.followers} / Following: {this.props.user.following}</p>
-                            <p><a href={this.props.user.html_url}>View details</a></p>
-                        </div>
-                    </CardContent>
-                </Card>
+            <div className="profile-info">
+                <div className="div-profile-card">
+                    <Card className="profile-card">
+                        <CardActionArea>
+                            <CardMedia
+                                className="card-media"
+                                image={this.props.user.avatar_url}
+                                title="User Profile Picture"
+                            />
+                            <CardContent>
+                            <Typography gutterBottom variant="h5" component="h2">
+                                {this.props.user.login}
+                            </Typography>
+                            <Typography component="p">
+                                {this.props.user.bio}
+                            </Typography>
+                            <br />
+                            <Typography component="p">
+                                Following: {this.props.user.following}
+                            </Typography>
+                            <Typography component="p">
+                                Followers: {this.props.user.followers}
+                            </Typography>
+                            </CardContent>
+                        </CardActionArea>
+                        <CardActions>
+                            <Button size="small" color="primary">
+                                View Details
+                            </Button>
+                        </CardActions>
+                        </Card>
+                </div>
                 <div>
                     <UserRepos
                         repos={this.props.repos}
--- src/components/userRepos/UserRepos.js
+++ src/components/userRepos/UserRepos.js
@@ -1,26 +1,48 @@
 import React, { Component } from 'react';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import './userRepos.css'
 
 class UserRepos extends Component {
     render() {
         var repos = this.props.repos.map((repo, key) => {
             return (
-                <div key={key} >
-                    <div>
-                        <h3>{repo.name}
-                            <span >{repo.stargazers_count} STARS</span>
-                        </h3>
-                        <p>{repo.description}</p>
-                        <p>
-                            <a href={repo.html_url}>Repository</a>
-                            <a href={repo.html_url + '/issues'}>Issues ({repo.open_issues}) </a>
-                        </p>
-                    </div>
-                </div>
+                    <Card className="card-items">
+                        <CardContent>
+                            <Typography variant="h5" component="h2">
+                                {repo.name}
+                            </Typography>
+                            <Typography component="p" color="textSecondary">
+                                {repo.description}
+                            </Typography>
+                        </CardContent>
+                        <CardActions>
+                            <Button size="small">View Details</Button>
+                        </CardActions>
+                        <CardActions>
+                            <Button size="small">Issues: {repo.open_issues}</Button>
+                        </CardActions>
+                    </Card>
+                // <div key={key} >
+                //     <div>
+                //         <h3>{repo.name}
+                //             <span >{repo.stargazers_count} STARS</span>
+                //         </h3>
+                //         <p>{repo.description}</p>
+                //         <p>
+                //             <a href={repo.html_url}>Repository</a>
+                //             <a href={repo.html_url + '/issues'}>Issues ({repo.open_issues}) </a>
+                //         </p>
+                //     </div>
+                // </div>
             );
         })
 
         return (
-            <div>
+            <div className="div-cards-repos">
                 {repos}
             </div>
         );
